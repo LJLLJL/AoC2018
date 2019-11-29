5da20ff87f221a78dbdba4f963cc3d7211c4b86e
LauriLe <lauri.lehtola@relex.fi>
2019-11-29 10:08:43 +0200

    uusi

--- aoc3_1.py
+++ aoc3_1.py
@@ -21,28 +21,32 @@ def set_canvas(input):
         data = split_data(i)
         w_temp = int(data[0][0]) + int(data[1][0])
         h_temp = int(data[0][1]) + int(data[1][1])
-        print(w_temp, h_temp)
+
         if w_temp > w_max:
             w_max = w_temp
 
         if h_temp > h_max:
             h_max = h_temp
-    #canvas = [[0 for x in range(w_max)] for y in range(h_max)]
-    canvas = np.zerors((w_max,h_max))
+    print(w_max, h_max)
+    canvas = np.zeros((w_max,h_max))
     return canvas
     #find max width and height
     #return array of correct size
 
-#def fll_canvas(input, canvas):
+def fill_canvas(input, canvas):
     #loop through input
+    for i in input:
+        canvas[0,0] +=1
     #increment each used
+    print(canvas)
 
 def main():
     input = read_input("aoc3_input.txt")
     print(input)
     print(split_data(input[0]))
-    canvas_dim = set_canvas(input)
-    print(canvas_dim)
+    canvas = set_canvas(input)
+    print(canvas)
+    fill_canvas(input,canvas)
 
 if __name__ == "__main__":
     main()
